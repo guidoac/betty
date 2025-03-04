1f6b3a87a0e2915ee570a828eb53300501dc892a
Guilherme <guido.ptr13@gmail.com>
2025-03-04 19:24:13 -0300

    messagem formatada

--- bet.py
+++ bet.py
@@ -12,52 +12,63 @@ class KebetBot():
         self.api_token = '7905621866:AAHdrCSjoVO2rm8Z-fJV-x5nY6e7TpvA_oU'
         self.chat_id = '5904792400'
         self.target = 'https://rtpkebet.com/#/home'
+        self.driver = None
+        self.page = None
+
+        self.initialize()
 
 
+    def initialize(self):
+        self.driver = webdriver.Chrome()
+        wait = WebDriverWait(self.driver, 5000)
+        self.driver.get(self.target)
+        self.page = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'gameBox')))
 
+    def getKebetElements(self):
+        games = self.page.find_elements(By.CLASS_NAME, 'gameItem')
 
+        for game in games:
+            name = game.find_element(By.CLASS_NAME, 'gameName')
+            items = game.find_elements(By.CLASS_NAME, 'processItem')
+            distribution = game.find_element(By.CLASS_NAME, 'arrow').get_attribute('src').startswith('https://rtpkebet.com/assets/down')
+            rtp = items[0].find_element(By.CLASS_NAME, 'el-progress--without-text').get_attribute('aria-valuenow')
+            min = items[1].find_element(By.CLASS_NAME, 'el-progress--without-text').get_attribute('aria-valuenow')
+            max = items[2].find_element(By.CLASS_NAME, 'el-progress--without-text').get_attribute('aria-valuenow')
+            avg = items[3].find_element(By.CLASS_NAME, 'el-progress--without-text').get_attribute('aria-valuenow')
+            oficial_rtp = items[4].find_element(By.CLASS_NAME, 'el-progress--without-text').get_attribute('aria-valuenow')
 
+            if (distribution and int(rtp) > 90):
+                self.slots.append({
+                    'name': name.text,
+                    'distribution': distribution,
+                    'RTP': rtp,
+                    'Min': min,
+                    'Max': max,
+                    'Avg': avg,
+                    'Oficial RTP': oficial_rtp,
+                })
 
+    def formatMessage(self):
+        message = ''
+        for slot in self.slots:
+            message += f"{slot['name']}: RTP {slot['RTP']}%\n"
+            message += f"Em distribuição? {'Sim' if slot['distribution'] else 'Não'}\n"
+            message += f"Min: {slot['Min']}%\n"
+            message += f"Max: {slot['Max']}%\n"
+            message += f"Avg: {slot['Avg']}%\n"
+            message += f"Oficial RTP: {slot['Oficial RTP']}%\n\n"
+            message += '---\n'
+        return message
 
-def getKebetElements():
-    driver = webdriver.Chrome()
-    wait = WebDriverWait(driver, 5000)
-    driver.get(TARGET_URL)
+    def sendMessage(self):
+        bot = Bot(token=self.api_token)
+        bot.send_message(chat_id=self.chat_id, text=self.formatMessage())
 
-    # Locate the element by CSS and print its content 
-    transcript = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'gameBox')))
-    games = transcript.find_elements(By.CLASS_NAME, 'gameItem')
+    def closeDriver(self):
+        self.driver.close()
 
-    for game in games:
-        name = game.find_element(By.CLASS_NAME, 'gameName')
-        items = game.find_elements(By.CLASS_NAME, 'processItem')
-        distribution = game.find_element(By.CLASS_NAME, 'arrow').get_attribute('src').startswith('https://rtpkebet.com/assets/down')
-        rtp = items[0].find_element(By.CLASS_NAME, 'el-progress--without-text').get_attribute('aria-valuenow')
-        min = items[1].find_element(By.CLASS_NAME, 'el-progress--without-text').get_attribute('aria-valuenow')
-        max = items[2].find_element(By.CLASS_NAME, 'el-progress--without-text').get_attribute('aria-valuenow')
-        avg = items[3].find_element(By.CLASS_NAME, 'el-progress--without-text').get_attribute('aria-valuenow')
-        oficial_rtp = items[4].find_element(By.CLASS_NAME, 'el-progress--without-text').get_attribute('aria-valuenow')
-
-        if (distribution and int(rtp) > 96):
-            slots.append({
-                'name': name.text,
-                'RTP': rtp,
-                'Min': min,
-                'Max': max,
-                'Avg': avg,
-                'Oficial RTP': oficial_rtp,
-            })
-
-    driver.close()
-
-def sendMessage(message):
-    bot = Bot(token=API_TOKEN)
-    bot.send_message(chat_id=CHAT_ID, text=message)
-
-
-def main():
-    while True:
-        getKebetElements()
-        print(str(slots))
-
-asyncio.run(main())
+while True:
+    kebet = KebetBot()
+    kebet.getKebetElements()
+    kebet.sendMessage()
+    kebet.closeDriver()
